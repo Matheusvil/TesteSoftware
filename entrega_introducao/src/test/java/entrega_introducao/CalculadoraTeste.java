2ba2a8e7b74b417dd999da38399b2d14f455290f
Matheusvil <matheusvilela65@hotmail.com>
2020-09-12 19:06:47 -0300

    :construction: TDD: Red, adicionando comentario no codigo

--- entrega_introducao/src/test/java/entrega_introducao/CalculadoraTeste.java
+++ entrega_introducao/src/test/java/entrega_introducao/CalculadoraTeste.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculadoraTeste {
+    
+    //Função teste onde realiza uma das funções basicas e verifica e o valor bate. Definimos o valor no asserEquals, onde o primeiro valor é o resultado esperado e o segundo é o valor é o obtido
+    
     @Test
     public void somarDoisValores() throws Exception {
         int valorA = 1;

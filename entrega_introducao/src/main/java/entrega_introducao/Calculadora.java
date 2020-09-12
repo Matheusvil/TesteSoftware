ad3910a81a0ecb2c5e53d4fdeddd3a415afcee3b
Matheusvil <matheusvilela65@hotmail.com>
2020-09-12 19:35:25 -0300

    :construction: TDD: Blue, arrumando o menu de seleção das operações

--- entrega_introducao/src/main/java/entrega_introducao/Calculadora.java
+++ entrega_introducao/src/main/java/entrega_introducao/Calculadora.java
@@ -32,8 +32,8 @@ public class Calculadora {
         System.out.println("-Escolha uma opção-");
         System.out.println("1. Soma");
         System.out.println("2. Subtração");
-        System.out.println("3. Divisão");
-        System.out.println("4. Multiplicação");
+        System.out.println("3. Multiplicação");
+        System.out.println("4. Divisão");
         System.out.println("0. Sair");
         System.out.println("Operação: ");
 

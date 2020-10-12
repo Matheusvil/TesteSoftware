15a33785b49cdf4bde9b9b379eb0a6724fa50f14
Matheusvil <matheusvilela65@hotmail.com>
2020-10-12 14:59:44 -0300

    TDD green - teste funcionais

--- entrega_teste_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/Calculadora.java
+++ entrega_teste_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/Calculadora.java
@@ -1,6 +1,98 @@
 package atividade_pratica_01;
 
+import java.util.Scanner;
+import org.apache.commons.math3.*;
+
 public class Calculadora {
 
-    
+    //Funções que realizam as contas basicas e foi adicionado um limite para o usuário escolher um numero entre 0 e 99.
+    public int soma(int valorA, int valorB) {
+        if(valorA<0 || valorA>99 & valorB<0 || valorB>99){
+            return 0;
+        }else{
+            return Math.addExact(valorA, valorB);
+        }
+    }
+
+    public int subtrai(int valorA, int valorB) {
+        if(valorA<0 || valorA>99 & valorB<0 || valorB>99){
+            return 0;
+        }else{
+            return Math.subtractExact(valorA, valorB);
+        }
+    }
+
+    public int divide(int valorA, int valorB) {
+        if(valorA<0 || valorA>99 & valorB<0 || valorB>99){
+            return 0;
+        }else{
+            return valorA / valorB;
+        }
+    }
+
+    public int multiplica(int valorA, int valorB) {
+        if(valorA<0 || valorA>99 & valorB<0 || valorB>99){
+            return 0;
+        }else{
+            return Math.multiplyExact(valorA, valorB);
+        }
+    }
+
+    //Função main onde o usuário interage
+    public static void main(String args[]) throws Exception {
+        Calculadora calc = new Calculadora();
+
+        int opc;
+        int valorA, valorB;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("-Escolha uma opção-");
+        System.out.println("1. Soma");
+        System.out.println("2. Subtração");
+        System.out.println("3. Multiplicação");
+        System.out.println("4. Divisão");
+        System.out.println("0. Sair");
+        System.out.println("Operação: ");
+
+        opc = input.nextInt();  
+        
+        while (opc != 0 && opc <= 4) {
+            Scanner input1 = new Scanner(System.in);    
+			
+			System.out.println("Qual o primeiro numero: ");  
+			valorA = input1.nextInt();  
+			System.out.println("Qual o segundo numero: ");  
+            valorB = input1.nextInt();
+
+
+            if(valorA<0 || valorA>99 & valorB<0 || valorB>99){
+                System.out.println("Qual o primeiro numero: ");  
+                valorA = input1.nextInt();  
+                System.out.println("Qual o segundo numero: ");  
+                valorB = input1.nextInt();
+            }  
+            else{
+                if ( opc == 1 ) {
+                    int operacao = calc.soma(valorA, valorB);
+                    System.out.printf("\nO resultado da soma é: %d\n", operacao);  
+                    break;  
+                }  
+                else if (opc == 2) { 
+                    int operacao = calc.subtrai(valorA, valorB);
+                    System.out.printf("\nO resultado da subtração é: %d\n", operacao);  
+                    break;  
+                }  
+                else if (opc == 3) {
+                    int operacao = calc.multiplica(valorA, valorB); 
+                    System.out.printf("\nO resultado da multiplicação é: %d\n", operacao);    
+                    break;  
+                }  
+                else{
+                    int operacao = calc.divide(valorA, valorB);  
+                    System.out.printf("\nO resultado da divisão é: %d\n", operacao);   
+                    break;  
+                } 
+            }
+        }
+    }
 }
--- entrega_teste_funcionais/atividade_pratica_01/src/test/java/atividade_pratica_01/CalculadoraTest.java
+++ entrega_teste_funcionais/atividade_pratica_01/src/test/java/atividade_pratica_01/CalculadoraTest.java
@@ -5,31 +5,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
-    //Função teste onde realiza uma das funções basicas e verifica se o valor bate. Definimos o valor no asserEquals, onde o primeiro valor é o resultado esperado e o segundo é o valor é o obtido.
+    //Função teste onde realiza uma das funções basicas e verifica e o valor bate. Definimos o valor no asserEquals, onde o primeiro valor é o resultado esperado e o segundo é o valor é o obtido
 
     @Test
     public void somarDoisValores1() throws Exception {
+        //Adicionado a classe Calculadora no teste.
+        Calculadora calculadora = new Calculadora();
         int valorA = 1;
         int valorB = 2;
-        int soma = 0;
+        int soma = calculadora.soma(valorA, valorB);
 
         assertEquals(3, soma);
     }
 
     @Test
     public void somarDoisValores2() throws Exception {
+        Calculadora calculadora = new Calculadora();
         int valorA = -10;
         int valorB = 2;
-        int soma = -8;
+        int soma = calculadora.soma(valorA, valorB);
 
         assertEquals(0, soma);
     }
 
     @Test
     public void somarDoisValores3() throws Exception {
+        Calculadora calculadora = new Calculadora();
         int valorA = 1;
         int valorB = 100;
-        int soma = 101;
+        int soma = calculadora.soma(valorA, valorB);
 
         assertEquals(0, soma);
     }
